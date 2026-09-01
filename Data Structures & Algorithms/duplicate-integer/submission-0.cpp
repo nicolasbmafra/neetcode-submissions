3391ed2a0f591af2d59c90157f872bce98ef42f1
Nicolas de Brito Mafra <90913641+nicolasbmafra@users.noreply.github.com>
2026-09-01 16:02:56 -0300

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        set<int> set;
+        for(int i=0;i<nums.size();i++) {
+            set.insert(nums[i]);
+        }
+        if (set.size() == nums.size()) {
+            return false;
+        }
+        return true;
+    }
+};
